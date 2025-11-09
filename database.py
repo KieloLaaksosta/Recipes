6912f086204d987c73ea52244c33f269e18a98eb
Joonatan Paalanen <joonatan.paalanen@helsinki.fi>
2025-11-09 10:56:21 +0200

    Make database.py functions use single connection for the entire function call

--- database.py
+++ database.py
@@ -5,35 +5,28 @@ def last_insert_id():
     return g.last_insert_id  
 
 def get_connection():
-    db = sqlite3.connect("database.db")
-    db.execute("PRAGMA foreign_keys = ON")
-    db.row_factory = sqlite3.Row
+    connection = sqlite3.connect("database.db")
+    connection.execute("PRAGMA foreign_keys = ON")
+    connection.row_factory = sqlite3.Row
 
-    return db
+    return connection
 
-def execute(query: str, params):
-    db = get_connection()
+def execute(query: str, params, connection):
     try:
-        result = db.execute(query, params)
+        result = connection.execute(query, params)
         g.last_insert_id = result.lastrowid
-        db.commit()
+        connection.commit()
     except Exception as e:
-        db.rollback()
-        db.close()
+        connection.rollback()
         raise e
 
-    db.close()
-
-def query(query: str, params):
-    db = get_connection()
+def query(query: str, params, connection):
     try:
-        result = db.execute(query, params).fetchall()
-        db.commit()
+        result = connection.execute(query, params).fetchall()
+        connection.commit()
     except Exception as e:
-        db.rollback()
-        db.close()
+        connection.rollback()
         raise e
-    db.close()
 
     return result
 
@@ -42,112 +35,156 @@ def get_placeholders(n: int) -> str:
     return ", ".join(placeholders)
 
 def add_account(username: str, hashed_password):
-    execute(
-        "INSERT INTO Users (Id, Username, PasswordHash) VALUES (NULL, ?, ?)",
-        [username, hashed_password]
-    )
+    connection = get_connection()
+
+    try:
+        execute(
+            "INSERT INTO Users (Id, Username, PasswordHash) VALUES (NULL, ?, ?)",
+            [username, hashed_password],
+            connection
+        )
+    finally:
+        connection.close()
 
 def get_password(username: str):
-    return query(
-        "SELECT PasswordHash FROM Users WHERE Username = ?",
-        [username], 
-    )
+    connection = get_connection()
+
+    try:
+        return query(
+            "SELECT PasswordHash FROM Users WHERE Username = ?",
+            [username], 
+            connection
+        )
+    finally:
+        connection.close()
 
 def get_user_id(username: str):
-    return query(
-        "SELECT Id FROM Users WHERE Username = ?",
-        [username], 
-    )
+    connection = get_connection()
+
+    try:
+        return query(
+            "SELECT Id FROM Users WHERE Username = ?",
+            [username], 
+            connection
+        )
+    finally:
+        connection.close()
 
 def add_recipe(creator, recipe_name: str, ingredients: str, instructions: str, tag_ids: list):
-    execute(
-        """
-        INSERT INTO Recipes (Id, Name, CreatorId, Instructions, Ingredients)
-        VALUES (NULL, ?, ?, ?, ?)
-        """,
-        [recipe_name, creator, ingredients, instructions]
-    )
+    connection = get_connection()
 
-    recipe_id = last_insert_id()
-    
-    for tag in tag_ids:
+    try:
         execute(
             """
-            INSERT INTO TagJoin (RecipeId, TagId)
-            VALUES (?, ?)
+            INSERT INTO Recipes (Id, Name, CreatorId, Instructions, Ingredients)
+            VALUES (NULL, ?, ?, ?, ?)
             """,
-            [recipe_id, tag]
+            [recipe_name, creator, ingredients, instructions],
+            connection
         )
 
+        recipe_id = last_insert_id()
+        
+        for tag in tag_ids:
+            execute(
+                """
+                INSERT INTO TagJoin (RecipeId, TagId)
+                VALUES (?, ?)
+                """,
+                [recipe_id, tag],
+                connection
+            )
+    finally:
+        connection.close()
+
 def get_available_tags():
-    return query("SELECT Id, Name FROM Tags ORDER BY Id", [])
+    connection = get_connection()
+
+    try:
+        return query("SELECT Id, Name FROM Tags ORDER BY Id", [], connection)
+    finally:
+        connection.close()
 
 def query_recipes(search: str, tag_ids: list):
-    if len(tag_ids) == 0:
+    connection = get_connection()
+
+    try:
+        if len(tag_ids) == 0:
+            return query(
+                """
+                SELECT
+                    R.Id, R.Name, U.Username As CreatorName, U.Id As CreatorId
+                FROM 
+                    Recipes AS R
+                    JOIN Users AS U ON U.Id = R.CreatorId
+                WHERE 
+                    (R.Name LIKE ? OR R.Instructions LIKE ? OR R.Ingredients LIKE ?)
+                """,
+                [
+                    search,
+                    search,
+                    search
+                ],
+                connection
+            )
         return query(
-            """
+            f"""
             SELECT
                 R.Id, R.Name, U.Username As CreatorName, U.Id As CreatorId
             FROM 
                 Recipes AS R
                 JOIN Users AS U ON U.Id = R.CreatorId
+                JOIN TagJoin TJ ON TJ.RecipeId = R.Id
             WHERE 
                 (R.Name LIKE ? OR R.Instructions LIKE ? OR R.Ingredients LIKE ?)
+                AND TJ.TagId IN ({get_placeholders(len(tag_ids))})
+            GROUP BY
+                R.Id
+            HAVING
+                COUNT(DISTINCT TJ.TagId) = ?
             """,
-            [
-                search,
-                search,
-                search
-            ]
+            3 * [search] + tag_ids + [len(tag_ids)],
+            connection
         )
-    return query(
-        f"""
-        SELECT
-            R.Id, R.Name, U.Username As CreatorName, U.Id As CreatorId
-        FROM 
-            Recipes AS R
-            JOIN Users AS U ON U.Id = R.CreatorId
-            JOIN TagJoin TJ ON TJ.RecipeId = R.Id
-        WHERE 
-            (R.Name LIKE ? OR R.Instructions LIKE ? OR R.Ingredients LIKE ?)
-            AND TJ.TagId IN ({get_placeholders(len(tag_ids))})
-        GROUP BY
-            R.Id
-        HAVING
-            COUNT(DISTINCT TJ.TagId) = ?
-        """,
-        3 * [search] + tag_ids + [len(tag_ids)]
-    )
+    finally:
+        connection.close()
 
 def get_recipe(recipe_id : int):
-    recipe = query(
-        """
-        SELECT
-            R.Id, R.Name, R.Ingredients, R.Instructions, U.Username As CreatorName, U.Id As CreatorId
-        FROM 
-            Recipes AS R
-            JOIN Users AS U ON U.Id = R.CreatorId
-        WHERE 
-            R.Id = ?
-        """,
-        [recipe_id]
-    )
+    connection = get_connection()
 
-    tags = query(
-        """
-        SELECT 
-            T.Name As TagName
-        FROM  
-            Tags AS T
-            JOIN TagJoin AS TJ ON T.Id = TJ.TagId
-        WHERE
-            TJ.RecipeId = ?
-        """,
-        [recipe_id]
+    try:
+        recipe = query(
+            """
+            SELECT
+                R.Id, R.Name, R.Ingredients, R.Instructions, U.Username As CreatorName, U.Id As CreatorId
+            FROM 
+                Recipes AS R
+                JOIN Users AS U ON U.Id = R.CreatorId
+            WHERE 
+                R.Id = ?
+            """,
+            [recipe_id],
+            connection
         )
-    
-    tag_names = []
-    for row in tags:
-        tag_names.append(row["TagName"])
-    
-    return (recipe, tag_names)
+
+        tags = query(
+            """
+            SELECT 
+                T.Name As TagName
+            FROM  
+                Tags AS T
+                JOIN TagJoin AS TJ ON T.Id = TJ.TagId
+            WHERE
+                TJ.RecipeId = ?
+            """,
+            [recipe_id],
+            connection
+        )
+        
+        tag_names = []
+        for row in tags:
+            tag_names.append(row["TagName"])
+        
+        return (recipe, tag_names)
+    finally:
+        connection.close()
