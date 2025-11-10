559fe9c88bac8423f3566d711c11dc5e750f7c37
Joonatan Paalanen <joonatan.paalanen@helsinki.fi>
2025-11-10 15:05:02 +0200

    Add Docker containerization

--- app.py
+++ app.py
@@ -2,84 +2,94 @@ from flask import Flask
 from flask import render_template, request, session, redirect
 import config, database, account
 
-app = Flask(__name__)
-app.secret_key = config.secret_key
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+    app.config.from_object(config)
 
-@app.route("/register")
-def register():
-    return render_template("register.html")
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+        app.secret_key = config.secret_key
+    else:
+        app.config.from_mapping(test_config)
 
-@app.route("/create_account", methods=["POST"])
-def create_account():
-    username = request.form["username"]
-    password = request.form["password"]
-    password_again = request.form["password_again"]
+    @app.route("/")
+    def index():
+        return render_template("index.html")
 
-    success_full, error_msg = account.try_create_account(username, password, password_again)
-    if not success_full:
-        return f"VIRHE: {error_msg}" + '   <a href="/register">yritä uudestaan</a>   <a href="/">palaa alkuun</a>'
-    
-    session["username"] = username
-    session["user_id"] = database.get_user_id(username)[0]["Id"]
-    return redirect("/")
+    @app.route("/register")
+    def register():
+        return render_template("register.html")
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
+    @app.route("/create_account", methods=["POST"])
+    def create_account():
+        username = request.form["username"]
+        password = request.form["password"]
+        password_again = request.form["password_again"]
 
-@app.route("/login_account", methods=["POST"])
-def login_account():
-    username = request.form["username"]
-    password = request.form["password"]
-
-    if account.check_password(username, password):
+        success_full, error_msg = account.try_create_account(username, password, password_again)
+        if not success_full:
+            return f"VIRHE: {error_msg}" + '   <a href="/register">yritä uudestaan</a>   <a href="/">palaa alkuun</a>'
+        
         session["username"] = username
         session["user_id"] = database.get_user_id(username)[0]["Id"]
         return redirect("/")
-    else:
-        return 'Väärä käyttäjätunnus tai salasana   <a href="/login">yritä uudestaan</a>   <a href="/">palaa alkuun</a>'
+
+    @app.route("/login")
+    def login():
+        return render_template("login.html")
+
+    @app.route("/login_account", methods=["POST"])
+    def login_account():
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if account.check_password(username, password):
+            session["username"] = username
+            session["user_id"] = database.get_user_id(username)[0]["Id"]
+            return redirect("/")
+        else:
+            return 'Väärä käyttäjätunnus tai salasana   <a href="/login">yritä uudestaan</a>   <a href="/">palaa alkuun</a>'
+        
+    @app.route("/logout")
+    def logout():
+        del session["username"]
+        return redirect("/")
+
+    @app.route("/create_recipe")
+    def create_recipe():
+        tags = database.get_available_tags()
+        return render_template("create_recipe.html", available_tags=tags)
+
+    @app.route("/add_recipe", methods=["POST"])
+    def add_recipe():
+        recipe_name = request.form["recipe_name"]
+        ingredients = request.form["ingredients"]
+        instructions = request.form["instructions"]
+
+        tag_names = request.form.getlist("tags")
+
+        database.add_recipe(session["user_id"], recipe_name, ingredients, instructions, tag_names)
+        return redirect("/")
+
+    @app.route("/search_recipe")
+    def search_recipe():
+        tags = database.get_available_tags()
+        return render_template("search_recipe.html", available_tags=tags)
+
+    @app.route("/query_recipes", methods=["POST"])
+    def query_recipes():
+        search = request.form["search"]
+        tag_names = request.form.getlist("tags")
+
+        results = database.query_recipes(search, tag_names)
+        return render_template("search_results.html", found_recipes = len(results), recipes=results)
+
+    @app.route("/recipes/<int:recipe_id>", methods=["GET"])
+    def show_recipe(recipe_id):
+        recipes, tag_names = database.get_recipe(recipe_id)
+        if(len(recipes) < 1):
+            return "Reseptiä ei löytynyt."
+        return render_template("recipe.html", recipe=recipes[0], tag_names=tag_names)
     
-@app.route("/logout")
-def logout():
-    del session["username"]
-    return redirect("/")
-
-@app.route("/create_recipe")
-def create_recipe():
-    tags = database.get_available_tags()
-    return render_template("create_recipe.html", available_tags=tags)
-
-@app.route("/add_recipe", methods=["POST"])
-def add_recipe():
-    recipe_name = request.form["recipe_name"]
-    ingredients = request.form["ingredients"]
-    instructions = request.form["instructions"]
-
-    tag_names = request.form.getlist("tags")
-
-    database.add_recipe(session["user_id"], recipe_name, ingredients, instructions, tag_names)
-    return redirect("/")
-
-@app.route("/search_recipe")
-def search_recipe():
-    tags = database.get_available_tags()
-    return render_template("search_recipe.html", available_tags=tags)
-
-@app.route("/query_recipes", methods=["POST"])
-def query_recipes():
-    search = request.form["search"]
-    tag_names = request.form.getlist("tags")
-
-    results = database.query_recipes(search, tag_names)
-    return render_template("search_results.html", found_recipes = len(results), recipes=results)
-
-@app.route("/recipes/<int:recipe_id>", methods=["GET"])
-def show_recipe(recipe_id):
-    recipes, tag_names = database.get_recipe(recipe_id)
-    if(len(recipes) < 1):
-        return "Reseptiä ei löytynyt."
-    return render_template("recipe.html", recipe=recipes[0], tag_names=tag_names)
+    return app
---
+++ dockerfile
@@ -0,0 +1,30 @@
+FROM python:3.14.0-slim-bookworm AS build
+
+WORKDIR /app
+
+RUN pip install build
+COPY . .
+RUN python -m build --wheel
+
+RUN apt-get update
+RUN apt-get install -y sqlite3
+RUN sqlite3 database.db < schema.sql
+RUN sqlite3 database.db < init.sql
+
+FROM python:3.14.0-slim-bookworm AS run
+
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY --from=build /app/dist/*.whl .
+
+RUN apt-get update
+RUN apt-get install -y sqlite3
+COPY --from=build /app/database.db .
+
+RUN pip install *.whl
+
+RUN pip install waitress
+
+ENTRYPOINT ["waitress-serve"]
+CMD ["--call", "recipes:create_app"]
