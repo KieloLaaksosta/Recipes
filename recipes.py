6425eff07f4b6780119e8bc45b753930aaf16d57
Joonatan Paalanen <joonatan.paalanen@helsinki.fi>
2025-11-29 16:03:47 +0200

    Reorder length parameters last

--- recipes.py
+++ recipes.py
@@ -9,7 +9,7 @@ def create_recipe_get():
         available_tags=tags,
         max_name_len=validation.MAX_RECIPE_NAME_LENGTH,
         max_ingredients_len=validation.MAX_INGREDIENTS_LENGTH,
-        max_instructions_len=validation.MAX_INSCTRUCTIONS_LENGTH
+        max_instructions_len=validation.MAX_INSTRUCTIONS_LENGTH
     )
 
 def create_recipe_post(recipe_name, ingredients, instructions, tag_ids):
@@ -26,7 +26,7 @@ def create_recipe_post(recipe_name, ingredients, instructions, tag_ids):
             error_msg = f"Reseptin nimen tulee olla vähintään {validation.MIN_RECIPE_NAME_LENGTH} merkkiä pitkä."
 
     _, ingredients = validation.limit_lenght(ingredients, max_len=validation.MAX_INGREDIENTS_LENGTH)
-    _, instructions = validation.limit_lenght(instructions, max_len=validation.MAX_INSCTRUCTIONS_LENGTH)
+    _, instructions = validation.limit_lenght(instructions, max_len=validation.MAX_INSTRUCTIONS_LENGTH)
 
     tag_ids = validation.truncate_list(tag_ids)
 
@@ -36,12 +36,12 @@ def create_recipe_post(recipe_name, ingredients, instructions, tag_ids):
             "create_recipe.html",
             error_msg=error_msg,
             available_tags=tags,
-            max_name_len=validation.MAX_RECIPE_NAME_LENGTH,
-            max_ingredients_len=validation.MAX_INGREDIENTS_LENGTH,
-            max_instructions_len=validation.MAX_INSCTRUCTIONS_LENGTH,
             name=recipe_name,
             ingredients=ingredients,
-            instructions=instructions
+            instructions=instructions,
+            max_name_len=validation.MAX_RECIPE_NAME_LENGTH,
+            max_ingredients_len=validation.MAX_INGREDIENTS_LENGTH,
+            max_instructions_len=validation.MAX_INSTRUCTIONS_LENGTH
         )
     database.add_recipe(session["user_id"], recipe_name, ingredients, instructions, tag_ids)
     return redirect("/")
@@ -74,8 +74,8 @@ def query_recipes_post(orginal_search: str, filter_tag_ids: list, page: int):
         did_search=True,
         search=orginal_search,
         filter_tag_ids=filter_tag_ids,
-        max_search_len=validation.MAX_SEARCH_LENGTH,
-        page=page
+        page=page,
+        max_search_len=validation.MAX_SEARCH_LENGTH
     )
 
 def edit_recipe_get(recipe_id: int):
@@ -90,7 +90,8 @@ def edit_recipe_get(recipe_id: int):
         added_tags=(tag["TagId"] for tag in added_tags),
         max_name_len=validation.MAX_RECIPE_NAME_LENGTH,
         max_ingredients_len=validation.MAX_INGREDIENTS_LENGTH,
-        max_instructions_len=validation.MAX_INSCTRUCTIONS_LENGTH,
+        max_instructions_len=validation.MAX_INSTRUCTIONS_LENGTH,
+        max_comment_len=validation.MAX_COMMENT_LENGTH
     )
 
 def edit_recipe_post(recipe_id: int, recipe_name: str, instructions: str, ingredients: str, added_tags: list):
@@ -107,7 +108,7 @@ def edit_recipe_post(recipe_id: int, recipe_name: str, instructions: str, ingred
             error_msg = f"Reseptin nimen tulee olla vähintään {validation.MIN_RECIPE_NAME_LENGTH} merkkiä pitkä."
 
     _, ingredients = validation.limit_lenght(ingredients, max_len=validation.MAX_INGREDIENTS_LENGTH)
-    _, instructions = validation.limit_lenght(instructions, max_len=validation.MAX_INSCTRUCTIONS_LENGTH)
+    _, instructions = validation.limit_lenght(instructions, max_len=validation.MAX_INSTRUCTIONS_LENGTH)
 
     added_tags = validation.truncate_list(added_tags)
     tags = database.get_available_tags()
@@ -134,7 +135,8 @@ def edit_recipe_post(recipe_id: int, recipe_name: str, instructions: str, ingred
         recipe=recipe,
         max_name_len=validation.MAX_RECIPE_NAME_LENGTH,
         max_ingredients_len=validation.MAX_INGREDIENTS_LENGTH,
-        max_instructions_len=validation.MAX_INSCTRUCTIONS_LENGTH,
+        max_instructions_len=validation.MAX_INSTRUCTIONS_LENGTH,
+        max_comment_len=validation.MAX_COMMENT_LENGTH
     )
 
 def delete(recipe_id: int):
