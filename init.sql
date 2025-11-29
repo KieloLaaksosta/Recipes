0cce1d96103a464149ad5458d30237690adb1c5e
Joonatan Paalanen <joonatan.paalanen@helsinki.fi>
2025-11-29 14:25:43 +0200

    Change default tags

--- init.sql
+++ init.sql
@@ -1,5 +1,5 @@
 INSERT INTO Tags (Id, Name) VALUES (1, "Vegaaninen");
-INSERT INTO Tags (Id, Name) VALUES (2, "Alle 10 Min");
+INSERT INTO Tags (Id, Name) VALUES (2, "Alle 20 Min");
 INSERT INTO Tags (Id, Name) VALUES (3, "Tiukkaan budjettiin");
 INSERT INTO Tags (Id, Name) VALUES (4, "Laktoositon");
 INSERT INTO Tags (Id, Name) VALUES (5, "Gluteeniton");
